54a97b12d8ab687e436ed79ff5a7325cfbd96d9d
jenna031125 <jenna031125@gmail.com>
2026-05-05 05:34:32 +1200

    Different NPC&Objects / player freeze while dialogue
    1. Player freezes while dialogue is playing (Big struggle here, explanation on gemini)
    2. Placed more NPCs and Objects
    3. If player got crowbar, she can open the door (uses $ for yarn spinner variable)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,25 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
-
     private Vector2 _movement;
-
     private Rigidbody2D _rb;
+
+    // We keep this public so you can see it working in the Inspector
+    public bool canMove = true;
+    private DialogueRunner _dialogueRunner;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        // Automatically finds your "Dialogue System" prefab's runner
+        _dialogueRunner = FindFirstObjectByType<DialogueRunner>();
     }
 
-    // Update is called once per frame
+    public void SetMovement(bool status)
+    {
+        canMove = status;
+        if (!canMove)
+        {
+            _rb.linearVelocity = Vector2.zero;
+        }
+    }
+
     void Update()
     {
-        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+        // AUTOMATION: If the runner isn't running, ensure the player is unfrozen
+        if (_dialogueRunner != null && !_dialogueRunner.IsDialogueRunning && !canMove)
+        {
+            SetMovement(true);
+        }
 
-        _rb.linearVelocity = _movement * _moveSpeed;
+        if (canMove)
+        {
+            _movement.x = InputManager.Movement.x;
+            _movement.y = InputManager.Movement.y;
+
+            if (_movement.sqrMagnitude < 0.01f)
+            {
+                _movement.x = Input.GetAxisRaw("Horizontal");
+                _movement.y = Input.GetAxisRaw("Vertical");
+            }
+
+            _rb.linearVelocity = _movement.normalized * _moveSpeed;
+        }
+        else
+        {
+            _rb.linearVelocity = Vector2.zero;
+        }
     }
 }
